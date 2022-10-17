8d3ea245917fb9e52ca546ed37b4e251e136e48f
Damien-Collot <dcollot76@gmail.com>
2022-10-17 17:35:20 +0200

    Feat : Initialisation du programme

--- main.c
+++ main.c
@@ -2,6 +2,32 @@
 #include "function.h"
 
 int main(){
+    printf("-----------TicTacToe-----------\n");
+    printf("Choissisez votre mode de jeu : \n");
+    printf("Joueur contre Joueur(1)\n");
+    printf("Joueur contre Ordinateur(2)\n");
+    printf("Ordinateur contre Ordinateur(3)\n");
+    int rep;
+    scanf("%d",&rep);
+
+    if (rep < 1 || rep > 3){
+        while (rep < 1 || rep > 3)
+        {
+            printf("Veuillez entrez une option valide !!!\n");
+            scanf("%d",&rep);
+        }
+    }
+    printf("------------------------------------\n");
+
+    if (rep == 1){
+        printf("Mode Joueur contre Joueur sélectionné\n");
+    } else if (rep == 2){
+        printf("Mode Joueur contre ordinateur sélectionné\n");
+    } else {
+        printf("Mode ordinateur contre ordinateur sélectionné\n");
+        printf("Entrez le nombre de simulation que vous souhaitez");
+        scanf("%d",&rep);
+    }
 
     int table[3][3] = 
     {
@@ -9,14 +35,6 @@ int main(){
     { 4 , 5 , 6},
     { 7 , 8 , 9}
     };
-
-    firstAffiche(table);
-
-    table[0][2] = 0;
-    table[0][1] = -1;
-    table[1][2] = 0;
-
-    afficheMatrice(table);
 
     return 1;
 
