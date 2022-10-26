7a2158f1cafd34d88aa08541e5aebc0a2ffec96d
Elogrz <102291890+Elogrz@users.noreply.github.com>
2022-10-26 10:26:32 +0200

    Update function.h

--- function.h
+++ function.h
@@ -5,8 +5,5 @@ bool win(int tab[3][3], int joueur);
 bool CheckCoup(int tab[3][3],int valCoup);
 bool CheckCoupComputer(int tab[3][3],int valCoup);
 bool win1(int tab[5][5], int joueur);
-bool CheckCoup1(int tab[5][5],int valCoup);
-bool CheckCoupComputer1(int tab[5][5],int valCoup);
-bool win2(int tab[5][5], int joueur);
-bool CheckCoup2(int tab[5][5],int valCoup);
-bool CheckCoupComputer2(int tab[5][5],int valCoup);
+bool win2(int tab[7][7], int joueur);
+
